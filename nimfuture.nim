f903a5119251512fab91577ea2b8256167f635f2
simongarisch <gatman946@gmail.com>
2020-12-28 17:27:32 +1100

    adding to nim examples

---
+++ nimfuture.nim
@@ -0,0 +1,11 @@
+# nim compile --run nimfuture.nim
+import asyncdispatch
+
+var future = newFuture[int]()
+doAssert(not future.finished)
+
+future.callback =
+    proc (future: Future[int]) =
+        echo("Future is no longer empty, ", future.read)
+
+future.complete(42)
--- nimjson.nim
+++ nimjson.nim
@@ -11,3 +11,12 @@ assert obj["username"].kind == json.JString
 assert obj["username"].str == "Bob"
 
 echo obj["username"].str
+
+
+assert parseJson("null").kind == JNull
+assert parseJson("true").kind == JBool
+assert parseJson("42").kind == JInt
+assert parseJson("3.14").kind == JFloat
+assert parseJson("\"Hi\"").kind == JString
+assert parseJson("""{ "key": "value" }""").kind == JObject
+assert parseJson("[1, 2, 3, 4]").kind == JArray
